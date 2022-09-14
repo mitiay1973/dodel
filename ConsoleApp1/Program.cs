338ba2a06f7766ce3dd1b92b4dd17697a2fd260c
Dima Sorokin <sorokindima36@gmail.com>
2022-09-14 18:11:36 +0300

    готовое

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,8 +13,16 @@ namespace ConsoleApp1
 
             Figuri[] figuri = new Figuri[]
             {
-
+                new Triangle (5,2,5),
+                new Circle(8),
+                new Rectangle(5,6)
             };
+            for(int i= 0; i < figuri.Length; i++)
+            {
+                figuri[i].Perimeter();
+                figuri[i].Area();
+                figuri[i].PrintFigure();
+            }
             Console.ReadKey();
         }
     }
@@ -36,88 +44,81 @@ namespace ConsoleApp1
     {
         public double perim;
         public double plosha;
+        public Rectangle(double a, double b)
+        {
+            x = a;
+            y = b;
+        }
         public override decimal Area()
         {
-            if(figura == "треугольник")
-            {
                 plosha = x * y;
-            }
             return (decimal)plosha;
         }
 
         public override decimal Perimeter()
         {
-            if (figura == "треугольник")
-            {
                 perim = (x + y) * 2;
-            }
             return (decimal)perim;
         }
 
         public override void PrintFigure()
         {
-            if (figura == "треугольник")
-            {
                 Console.WriteLine($"Прямоугольник: Периметр = {perim}, Площадь = {plosha}");
-            }
         }
     }
     public class Circle : Figuri
     {
         public double perim;
         public double plosha;
+
+        public Circle(double rr)
+        {
+            r = rr;
+        }
         public override decimal Area()
         {
-            if (figura == "круг")
-            {
-                plosha = x * y;
-            }
+                plosha = Math.PI * Math.Pow(r,2);
             return (decimal)plosha;
         }
 
         public override decimal Perimeter()
         {
-            if (figura == "круг")
-            {
-                perim = (x + y) * 2;
-            }
+                perim = 2* Math.PI*r;
             return (decimal)perim;
         }
         public override void PrintFigure()
         {
-            if (figura == "круг")
-            {
                 Console.WriteLine($"Круг: Периметр = {perim}, Площадь = {plosha}");
-            }
         }
     }
     public class Triangle : Figuri
     {
         public double perim;
         public double plosha;
-        public override decimal Area()
+
+    public Triangle(double a, double b, double c)
+    {
+            x = a;
+            y = b;
+            z = c;
+    }
+
+
+    public override decimal Area()
         {
-            if (figura == "треугольник")
-            {
-                plosha = x * y;
-            }
+            plosha = Math.Sqrt(perim*(perim-x)* (perim - y)* (perim - z));
             return (decimal)plosha;
         }
 
         public override decimal Perimeter()
         {
-            if (figura == "треугольник")
-            {
-                perim = (x + y) * 2;
-            }
+
+            perim = x + y + z;
             return (decimal)perim;
         }
         public override void PrintFigure()
         {
-            if (figura == "треугольник")
-            {
                 Console.WriteLine($"Треугольник: Периметр = {perim}, Площадь = {plosha}");
-            }
         }
     }
 }
